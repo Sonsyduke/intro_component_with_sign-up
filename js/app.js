55a9f977e5d6a1ff4916427f6805931ea5e2d119
Douglas <douglaslombard29@gmail.com>
2022-04-04 16:34:50 +0200

    finished logic for the javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,64 @@
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  //trim to remove whitespaces
+
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "First name cannot be blank");
+  } else {
+    setSuccessFor(firstName);
+  }
+
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "Last name cannot be blank");
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Not a valid email address");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formGroup = input.parentElement;
+  const small = formGroup.querySelector(".error-message");
+  formGroup.className = "form-group error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formGroup = input.parentElement;
+  formGroup.className = "form-group success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
